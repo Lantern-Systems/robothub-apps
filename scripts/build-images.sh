b9203b9e88e717f3e3edfb02fcb9b40aa00831ad
chad-green <chad.green@lanternlabs.com>
2022-05-26 22:45:03 -0400

    test multi_cam_support-develop branch of depthai for ar0234 support

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -4,15 +4,14 @@ set -Eeuo pipefail
 if [[ -z "${DEPTHAI_BRANCH}" ]]; then
   DEPTHAI_BRANCH="main"
 fi
-ALPINE_TAG="ghcr.io/luxonis/robothub-base-app:alpine-depthai-${DEPTHAI_BRANCH}"
-UBUNTU_TAG="ghcr.io/luxonis/robothub-base-app:ubuntu-depthai-${DEPTHAI_BRANCH}"
-LATEST_TAG="ghcr.io/luxonis/robothub-base-app:latest"
+ALPINE_TAG="localhost:5000/luxonis/robothub-base-app:alpine-depthai-${DEPTHAI_BRANCH}"
+UBUNTU_TAG="localhost:5000/luxonis/robothub-base-app:ubuntu-depthai-${DEPTHAI_BRANCH}"
 
 echo "Building alpine..."
 # Alpine
 DOCKER_BUILDKIT=1 docker buildx \
   build \
-  --builder remotebuilder \
+  --builder localbuilder \
   --platform linux/arm64/v8,linux/amd64 \
   --build-arg DEPTHAI_BRANCH=${DEPTHAI_BRANCH} \
   -t $ALPINE_TAG \
@@ -24,15 +23,10 @@ echo "Building ubuntu..."
 #Ubuntu
 DOCKER_BUILDKIT=1 docker buildx \
   build \
-  --builder remotebuilder \
+  --builder localbuilder \
   --platform linux/arm64/v8,linux/amd64 \
   --build-arg DEPTHAI_BRANCH=${DEPTHAI_BRANCH} \
   -t $UBUNTU_TAG \
   --push \
   --file ./robothub_sdk/docker/ubuntu/Dockerfile \
   ./robothub_sdk
-
-if [[ "${DEPTHAI_BRANCH}" == "main" ]]; then
-  docker tag "$ALPINE_TAG" "$LATEST_TAG"
-  docker push "$LATEST_TAG"
-fi
